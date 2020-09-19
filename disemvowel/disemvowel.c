8209391f8cc06200ebef1136e86a9db44677aa78
robi1467 <robi1467@morris.umn.edu>
2020-09-18 20:27:07 -0500

    Made changes to allow file to pass tests
    I would like to note that I mistaken a change that was need earlier with the j++ it was in the right spot. In the code I made afew change mostly moving to the use of
    functions when ever possible also a few places to clean up code. The most notable change was I got rid of the giant if statement in favor of a function.
    This commit also saw the creation of a counter to get the size of the new String... I miss Java `String hello ="bye"; System.out.println(hello);

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,19 +1,37 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "disemvowel.h"
 
+bool checkForVowel(char letter){
+return( letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' ||
+    letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U' );
+}
+int getNewSize(char *str,int len){
+	int count = 0;
+	int i;
+	for(i=0; i<len; i++){
+		if(!checkForVowel(str[i])){
+		count++;
+		}
+	}
+	return count;
+}
 char *disemvowel(char *str) {
 	int len = strlen(str);
-	int j = 0;
 	char* output;
-        output = (char*)calloc(strlen(str)+1, sizeof(char));
-	for(int i=0;i<len;i++){
-		if(str[i]!= 'a' || str[i]!= 'e' || str[i]!= 'i' || str[i]!= 'o' || str[i]!= 'u' || str[i]!= 'A' || str[i]!= 'E' || str[i]!= 'I' || str[i]!= 'O' || str[i]!= 'U'){
+	
+	int newSize = getNewSize(str,len);
+        output = (char*)calloc(newSize+1, sizeof(char));
+	
+	int i,j;
+	for(i=0; i<len; i++){	
+		if(!checkForVowel(str[i])){
 			output[j] = str[i];
+			j++;
 		}
-		j++;
 	}
-	output[j] = '\0';
+	output[newSize] = '\0';
 	
   return output;
 }
